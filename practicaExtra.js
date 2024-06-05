4958880acd896fcfb892eacc9d97b870d7169db0
Jose3004 <119958493+Jose3004@users.noreply.github.com git config --global user.email "119958493+Jose3004@users.noreply.github.com>
2024-06-05 07:48:12 -0500

    Archivo adicional JS para la practica

---
+++ practicaExtra.js
@@ -0,0 +1,15 @@
+//Este archivo esta creado especificamente para ampliar las sesiones de practica de todo lo que he visto hasta el momento durante el curso. 
+//Buscaré ejercicios en la web donde me ponga a prueba y amplie mi logica, para mejorar mi codigo y asi poco a poco construir lineas más complejas.
+
+/* ----------------------------- Variables ----------------------------- */
+//Ejercicio 1
+//Declara una variable llamada nombre en tu consola y asignale tu nombre.
+let miNombre = "Jose  Samuel";
+//Declara una variable llamada apellido y asignale como valor tu apellido.
+let miapellido;
+miapellido = "Castro  Ospina";
+//Muestra por consola las variables nombre y apellido.
+let nombreCompleto = miNombre   +   miapellido;
+console.log(nombreCompleto);
+
+//Ejercicio 2
